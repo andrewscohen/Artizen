d27deb158696e0f0dc76495f1a662225f6244800
rob <darabandev@gmail.com>
2021-02-22 16:23:22 -0500

    Working on navbar

---
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <>
+            <input type="text" placeholder="Enter a city" />
+            <button type="submit">Search</button>
+        </>
+    )
+}
+
+export default SearchBar;
