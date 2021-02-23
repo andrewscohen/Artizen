e901a448b1065066f354ccb69e5ed504a6f65ae2
Austin <austin.smithfield@gmail.com>
2021-02-23 23:53:44 +0000

    edited form page

--- app/forms/new_location_form.py
+++ app/forms/new_location_form.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired, Email, ValidationError
 class LocationForm(FlaskForm):
     title=StringField('title')
     artist=StringField('artist')
-    street_address=StringField('street_address', validators=[DataRequired])
-    city=StringField('city', validators=[DataRequired])
-    state=StringField('state', validators=[DataRequired])
-    zip_code=IntegerField('zip_code', validators=[DataRequired])
+    street_address=StringField('street_address', validators=[DataRequired()])
+    city=StringField('city', validators=[DataRequired()])
+    state=StringField('state', validators=[DataRequired()])
+    zip_code=IntegerField('zip_code', validators=[DataRequired()])
     description=TextField('description')
     
