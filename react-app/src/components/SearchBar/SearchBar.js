829edceff52c95f2a8e63a78e2b94df813b76159
Andrew <andrewcohen14@gmail.com>
2021-02-24 17:15:39 -0500

    Map File Clean Up

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./SearchBar.css";
+// import Search from '../Maps/Search.js'
 
 const SearchBar = () => {
     return (
         <div className="search-wrapper">
+            {/* <Search /> */}
             <input type="text" placeholder="Enter a city" />
             <button type="submit">
                 <i className="fas fa-search"></i>
