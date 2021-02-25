ea7aecf194a9f147201b39c717a44ef3dfd09eee
rob <darabandev@gmail.com>
2021-02-24 23:48:24 -0500

    Reorganized html elements for better layout

--- react-app/src/components/Menu/profilebutton.js
+++ react-app/src/components/Menu/profilebutton.js
@@ -1,65 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import LogoutButton from '../auth/LogoutButton';
-import './menu.css';
+import LogoutButton from "../auth/LogoutButton";
+import "./menu.css";
 
-export default function ProfileButton({setAuthenticated}) {
+export default function ProfileButton({ setAuthenticated }) {
+  const [showMenu, setShowMenu] = useState(false);
 
-    const [showMenu, setShowMenu] = useState(false);
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
     };
 
-    useEffect(() => {
-      if (!showMenu) return;
+    document.addEventListener("click", closeMenu);
 
-      const closeMenu = () => {
-        setShowMenu(false);
-      };
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-      document.addEventListener('click', closeMenu);
-
-      return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
-
-    return (
-      <>
-        <button className="btn btn--hamburger" onClick={openMenu}>
-          <i className="fas fa-bars"></i>
-        </button>
-        {showMenu && (
-        <ul className="dropdown">
+  return (
+    <>
+      <button className="btn btn--hamburger" onClick={openMenu}>
+        <i className="fas fa-bars"></i>
+      </button>
+      {showMenu && (
+        <div className="dropdown">
           <li className="dropdown__li dropdown__header dropdown__header--background">User Name to Go Here</li>
           <li className="dropdown__li dropdown__header--email dropdown__header--background">User Email to go here</li>
           <li className="dropdown__li">
             <Link to="/dashboard">
-              <button className="btn--dropdown" >
-                Dashboard
-              </button>
+              <button className="btn--dropdown">Dashboard</button>
             </Link>
           </li>
           <li className="dropdown__li">
             <Link to="/locations/new">
-              <button className="btn--dropdown" >
-                Add an Art Location
-              </button>
+              <button className="btn--dropdown">Add an Art Location</button>
             </Link>
           </li>
           <li className="dropdown__li">
             <Link to="/artwalks/new">
-              <button className="btn--dropdown" >
-                Create an Art Walk
-              </button>
+              <button className="btn--dropdown">Create an Art Walk</button>
             </Link>
           </li>
           <li className="dropdown__li">
-          <LogoutButton className="btn--dropdown" setAuthenticated={setAuthenticated} />
+            <LogoutButton className="btn--dropdown" setAuthenticated={setAuthenticated} />
           </li>
-        </ul>
-        )}
-      </>
-    )
+        </div>
+      )}
+    </>
+  );
 }
-
