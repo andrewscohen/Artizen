ad31ff76a59d623069f9846bda0ab39ff19d65c7
rob <darabandev@gmail.com>
2021-02-24 23:58:22 -0500

    Dropdown layout looks good

--- react-app/src/components/Menu/profilebutton.js
+++ react-app/src/components/Menu/profilebutton.js
@@ -30,26 +30,38 @@ export default function ProfileButton({ setAuthenticated }) {
       </button>
       {showMenu && (
         <div className="dropdown">
-          <li className="dropdown__li dropdown__header dropdown__header--background">User Name to Go Here</li>
-          <li className="dropdown__li dropdown__header--email dropdown__header--background">User Email to go here</li>
-          <li className="dropdown__li">
-            <Link to="/dashboard">
-              <button className="btn--dropdown">Dashboard</button>
-            </Link>
-          </li>
-          <li className="dropdown__li">
-            <Link to="/locations/new">
-              <button className="btn--dropdown">Add an Art Location</button>
-            </Link>
-          </li>
-          <li className="dropdown__li">
-            <Link to="/artwalks/new">
-              <button className="btn--dropdown">Create an Art Walk</button>
-            </Link>
-          </li>
-          <li className="dropdown__li">
-            <LogoutButton className="btn--dropdown" setAuthenticated={setAuthenticated} />
-          </li>
+          <div>
+            <li className="dropdown__li dropdown__header dropdown__header--background">User Name to Go Here</li>
+          </div>
+          <div>
+            <li className="dropdown__li dropdown__header--email dropdown__header--background">User Email to go here</li>
+          </div>
+          <div>
+            <li className="dropdown__li">
+              <Link to="/dashboard">
+                <button className="btn--dropdown">Dashboard</button>
+              </Link>
+            </li>
+          </div>
+          <div>
+            <li className="dropdown__li">
+              <Link to="/locations/new">
+                <button className="btn--dropdown">Add an Art Location</button>
+              </Link>
+            </li>
+          </div>
+          <div>
+            <li className="dropdown__li">
+              <Link to="/artwalks/new">
+                <button className="btn--dropdown">Create an Art Walk</button>
+              </Link>
+            </li>
+          </div>
+          <div>
+            <li className="dropdown__li">
+              <LogoutButton className="btn--dropdown" setAuthenticated={setAuthenticated} />
+            </li>
+          </div>
         </div>
       )}
     </>
