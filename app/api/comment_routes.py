319f4091795aff1e27a067f283996d6903e5e630
rob <darabandev@gmail.com>
2021-02-26 16:12:15 -0500

    Added thunk to add new comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify, json
+from flask import Blueprint, jsonify, json, request
 from flask_login import login_required
 from app.models import Comment, Location, db
+from app.forms.comment_form import CommentForm
 from datetime import datetime
 
 comment_routes = Blueprint("comments", __name__)
@@ -14,16 +15,26 @@ def comments(location_id):
     return json.dumps(data)
 
 
-@comment_routes.route("/new/<int:location_id>/<int:user_id>", methods=["POST"])
+@comment_routes.route("/new/<int:location_id>/", methods=["POST"])
 @login_required
 def add_comment(location_id, user_id):
-    comment = Comment(
-        user_id=user_id,
-        location_id=location_id,
-        created_at=datetime.now(),
-        updated_at=datetime.now()
-    )
+    form = CommentForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
-    db.session.add(comment)
+    if form.validate_on_submit():
+        comment = Comment(
+            user_id=user_id,
+            location_id=location_id,
+            comment=form.data["comment"],
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
 
-    db.session.commit()
+        db.session.add(comment)
+
+        db.session.commit()
+
+        comments = Comment.query.filter(
+            Comment.location_id == location_id).all()
+        data = [comment.to_dict() for comment in comments]
+        return json.dumps(data)
--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -1,8 +1,16 @@
 const GET_LOCATION_COMMENTS = "comments/GET_LOCATION_COMMENTS";
+const NEW_COMMENT = "comments/NEW_COMMENT";
 
 const getCommentsForLocation = comments => {
   return {
     type: GET_LOCATION_COMMENTS,
+    payload: comments,
+  };
+};
+
+const addNewCommentToLocation = comments => {
+  return {
+    type: NEW_COMMENT,
     payload: comments,
   };
 };
@@ -14,11 +22,30 @@ export const getComments = locationId => async dispatch => {
   dispatch(getCommentsForLocation(data));
 };
 
+export const addComment = (comment, locationId, userId) => async dispatch => {
+  const res = await fetch(`/api/comments/new/${locationId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      comment,
+      locationId,
+      userId,
+    }),
+  });
+
+  const data = await res.json();
+  dispatch(addNewCommentToLocation(data));
+};
+
 const initialState = [];
 
 const commentsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_LOCATION_COMMENTS:
+      return action.payload;
+    case NEW_COMMENT:
       return action.payload;
     default:
       return state;
