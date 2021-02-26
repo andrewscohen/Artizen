604e9047327110cb794f1078b2ac602c6d8cf81c
Andrew <andrewcohen14@gmail.com>
2021-02-26 16:46:21 -0500

    Working Directions Component

--- react-app/src/components/Maps/DirectionsMap.js
+++ react-app/src/components/Maps/DirectionsMap.js
@@ -1,15 +1,19 @@
-import {useState} from "react";
-import {GoogleMap, useLoadScript, DirectionsService, DirectionsRenderer} from "@react-google-maps/api";
+import React, {useState} from "react";
+import {GoogleMap, DirectionsService, DirectionsRenderer, LoadScript, Marker} from "@react-google-maps/api";
 
-const libraries = ["places"];
+// const libraries = ["places"];
 
 const Directions = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_PLACES_API_KEY, libraries
-  }, libraries);
+  // const { isLoaded, loadError } = useLoadScript({
+  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_PLACES_API_KEY,
+  //   libraries ,
+  // });
 
   const [response, setResponse] = useState(null);
-  const [travelMode, setTravelMode] = useState('DRIVING');
+  // const [travelMode, setTravelMode] = useState('DRIVING');
+  const [markers, setMarkers] = useState([]);
+  const [services, setServices] = useState(true)
+
 
   const directionsCallback = (response) => {
     console.log("RESPONSE: ", response)
@@ -17,6 +21,7 @@ const Directions = () => {
     setTimeout(() => {
     if (response !== null) {
       if (response.status === 'OK') {
+        setServices(false)
         setResponse(response)
       } else {
         console.log('RESPONSE NOT OK: ', response)
@@ -25,32 +30,51 @@ const Directions = () => {
     }, 1000);
   }
 
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
+  // if (loadError) return "Error";
+  // if (!isLoaded) return "Loading...";
 
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
 
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
   return (
+<LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_PLACES_API_KEY}>
         <GoogleMap
           id='direction-example'
           mapContainerStyle={{
             height: '400px',
             width: '100%'
           }}
-          zoom={2}
+          zoom={8}
           center={{
-            lat: 0,
-            lng: -180,
-          }}>
+            lat: 39.37,
+            lng: -76.59,
+          }}
+          onClick={onMapClick}
+          onLoad={onMapLoad}
+        >
+          { services &&
               <DirectionsService
                 options={{
                   destination: "12521 Valley Pines Drive, Reisterstown, MD 21136",
                   origin: "900 Overbrook Road, Baltimore, MD 21239",
-                  travelMode: travelMode
+                  travelMode: "DRIVING"
                 }}
                 callback={directionsCallback}
               />
-
+}
           {
             response !== null && (
               <DirectionsRenderer
@@ -62,6 +86,7 @@ const Directions = () => {
             )
           }
         </GoogleMap>
+        </LoadScript>
   )
 }
 
