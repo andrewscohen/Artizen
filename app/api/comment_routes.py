5a5c374290162cd1e94d5df6175730b7006d2793
rob <darabandev@gmail.com>
2021-02-26 15:48:52 -0500

    Added route for new comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, json
 from flask_login import login_required
-from app.models import Comment, Location
+from app.models import Comment, Location, db
+from datetime import datetime
 
 comment_routes = Blueprint("comments", __name__)
 
@@ -11,3 +12,18 @@ def comments(location_id):
     comments = Comment.query.filter(Comment.location_id == location_id).all()
     data = [comment.to_dict() for comment in comments]
     return json.dumps(data)
+
+
+@comment_routes.route("/new/<int:location_id>/<int:user_id>", methods=["POST"])
+@login_required
+def add_comment(location_id, user_id):
+    comment = Comment(
+        user_id=user_id,
+        location_id=location_id,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+    db.session.add(comment)
+
+    db.session.commit()
