7572e06e8c699db64fbcac4f6549eba263666f52
Matt Kufchak <matt.kufchak@gmail.com>
2021-02-28 01:02:26 -0600

    added a file

---
+++ app/api/artwalk_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import ArtWalk
+import json
+
+artwalk_routes = Blueprint('artwalks', __name__)
+
+@artwalk_routes.route('/<int:id>')
+@login_required
+def artwalk(id):    
+    artwalk = ArtWalk.query.get(id)
+    data = artwalk.to_dict()
+    res = json.dumps(data)
+    return res
