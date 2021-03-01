1457ffff7516db9c791cd9e89113281bec04dbd8
Matt Kufchak <matt.kufchak@gmail.com>
2021-03-01 03:37:29 -0600

    more styles

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 // import { Redirect } from "react-router-dom";
 import ArtwalkContainer from "../ArtwalkContainer";
@@ -8,6 +9,7 @@ import "./UserProfile.css";
 export default function UserProfile() {
   const dispatch = useDispatch();
   const [change, setChange] = useState(false);
+  const [artwalks, setArtwalks] = useState(true);
   const [loaded, setLoaded] = useState(false);
   const sessionUser = useSelector(state => state.session.user);
   const { userArtwalks } = useSelector(state => state.artwalks);
@@ -18,6 +20,14 @@ export default function UserProfile() {
     dispatch(artwalkActions.getUserArtwalks(sessionUser.id));
   }, [dispatch, sessionUser, change]);
 
+  const showArtwalks = () => {
+    setArtwalks(true)
+  }
+
+  const showLocations = () => {
+    setArtwalks(false)
+  }
+
   // useEffect(()=> {
   //   setLoaded(true)
   // }, [artwalksArray])
@@ -26,7 +36,13 @@ export default function UserProfile() {
 
   return (
     <div className="main">
-      <h1>User Profile {sessionUser.id}</h1>
+      <div className="profile_nav">
+        <h1 className="profile_title">Welcome, {sessionUser.first_name}!</h1>
+        <div className="profile_nav-links">
+          <button className="profile_nav-btn" onClick={showArtwalks}>Your Art Walks</button>
+          <button className="profile_nav-btn" onClick={showLocations}>Your Art Locations</button>
+        </div>
+      </div>
       {artwalksArray.length > 0 && (
         <div className="user_main">
           {artwalksArray.map(artwalk => {
