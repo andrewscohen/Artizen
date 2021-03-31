4530c442dff48dba3b357d93e349ea3983fa5c5c
Andrew <andrewcohen14@gmail.com>
2021-03-30 23:55:31 -0400

    Updated Edit Modal to click on close, added accessibility element to app

--- react-app/src/components/LocationFormModal/location_form_modal.js
+++ react-app/src/components/LocationFormModal/location_form_modal.js
@@ -6,6 +6,7 @@ import { getCoords } from "../../services/maps";
 import "./LocationFormModal.css";
 
 
+// Modal Styling
 const customStyles = {
     content: {
         top: "50%",
@@ -31,7 +32,7 @@ const customStyles = {
 
 const LocationFormModal = ({showContainer, setShowContainer, setBackgroundStyle}) => {
 
-  const [modalIsOpen, setIsOpen] = useState(false);
+  // State variables
   const [title, setTitle] = useState("");
   const [artist, setArtist] = useState("");
   const [street_address, setAddress] = useState("");
@@ -41,25 +42,36 @@ const LocationFormModal = ({showContainer, setShowContainer, setBackgroundStyle}
   const [description, setDescription] = useState("");
   const [photo, setPhoto] = useState("");
   const [errors, setErrors] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-  const sessionUser = useSelector(state => state.session.user);
-
+  // Hook Variable Assignments
   const dispatch = useDispatch();
 
-  function openModal() {
+  // State Info
+  const sessionUser = useSelector(state => state.session.user);
+
+
+  // FUNCTIONS TO BE PASSED INTO RETURN
+
+  // Passed to onClick to open location form modal
+  const openModal = () => {
     setIsOpen(true);
     setShowContainer(false);
     setBackgroundStyle({backgroundColor: "rgba(0, 0, 0, 0.0)"})
   }
 
-  function closeModal() {
+  // Passed to onClick to close location form modal
+  const closeModal = () => {
     setIsOpen(false);
     setShowContainer(true);
     setBackgroundStyle({backgroundColor: "rgba(0, 0, 0, 0.7)"})
-
   }
-  //   const history = useHistory();
 
+  // Executed once form is submit.
+  // Coerces regular address into geocoded values for Google Maps API
+  // Sends Input information to addLocation thunk in store
+  // Returns Errors if any
+  // Clears form by reseting local state variable
   const handleSubmit = async e => {
     e.preventDefault();
     let newErrors = [];
@@ -101,75 +113,105 @@ const LocationFormModal = ({showContainer, setShowContainer, setBackgroundStyle}
   return (
     <>
       <div className="modal-container">
-      {showContainer && (
-      <button
-        className="login-btn"
-        onClick={openModal}>
-        Upload It Here
-      </button>
-      )}
-      <Modal
-        style={customStyles}
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-          >
-        <button className="btn__x" onClick={closeModal}>
-          <i className="fas fa-times"></i>
-        </button>
-        <div className="new-location-form">
-          <h1>Add Location</h1>
-            {errors.length > 0 && errors.map(error => <div className="errors" key={error}>{error}</div>)}
+        {showContainer && (
+          <button
+            className="login-btn"
+            onClick={openModal}>
+            Upload It Here
+          </button>
+        )}
+        <Modal
+          style={customStyles}
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+            >
+          <button className="btn__x" onClick={closeModal}>
+            <i className="fas fa-times" />
+          </button>
+          <div className="new-location-form">
+            <h1>Add Location</h1>
+              {errors.length > 0 && errors.map(error => <div className="errors" key={error}>{error}</div>)}
             <form onSubmit={handleSubmit}>
-            <div>
+              <div>
                 <label>Artwork Title</label>
-                <input type="text" placeholder="Title (optional)" value={title} onChange={e => setTitle(e.target.value)} />
-            </div>
-            <div>
+                <input
+                  type="text"
+                  placeholder="Title (optional)"
+                  value={title}
+                  onChange={e => setTitle(e.target.value)} />
+              </div>
+              <div>
                 <label>Artist Name</label>
-                <input type="text" placeholder="Artist (optional)" value={artist} onChange={e => setArtist(e.target.value)} />
-            </div>
-            <div>
+                <input
+                  type="text"
+                  placeholder="Artist (optional)"
+                  value={artist}
+                  onChange={e => setArtist(e.target.value)}
+                />
+              </div>
+              <div>
                 <label>Street Address</label>
                 <input
-                type="text"
-                placeholder="Street Address"
-                value={street_address}
-                onChange={e => setAddress(e.target.value)}
-                required
+                  type="text"
+                  placeholder="Street Address"
+                  value={street_address}
+                  onChange={e => setAddress(e.target.value)}
+                  required
                 />
-            </div>
-            <div>
+              </div>
+              <div>
                 <label>City</label>
-                <input type="text" placeholder="City" value={city} onChange={e => setCity(e.target.value)} required/>
-            </div>
-            <div>
+                <input
+                  type="text"
+                  placeholder="City"
+                  value={city}
+                  onChange={e => setCity(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
                 <label>State</label>
-                <input type="text" placeholder="State" value={state} onChange={e => setState(e.target.value)} required/>
-            </div>
-            <div>
+                <input
+                  type="text"
+                  placeholder="State"
+                  value={state}
+                  onChange={e => setState(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
                 <label>Zip Code</label>
-                <input type="number" placeholder="Zip Code" value={zip_code} onChange={e => setZip(e.target.value)} required/>
-            </div>
-            <div>
+                <input
+                  type="number"
+                  placeholder="Zip Code"
+                  value={zip_code}
+                  onChange={e => setZip(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
                 <label>Description</label>
                 <input
-                type="textarea"
-                placeholder="Description (optional)"
-                value={description}
-                onChange={e => setDescription(e.target.value)}
+                  type="textarea"
+                  placeholder="Description (optional)"
+                  value={description}
+                  onChange={e => setDescription(e.target.value)}
                 />
-            </div>
-            <div>
+              </div>
+              <div>
                 <label>Upload A Photo </label>
-                <input type="file" onChange={e => setPhoto(e.target.files[0])} required/>
-            </div>
-
-            <div>
-             <button type="submit">Create Location</button>
-            </div>
-          </form>
-        </div>
-      </Modal>
+                <input
+                  type="file"
+                  onChange={e => setPhoto(e.target.files[0])}
+                  required
+                />
+              </div>
+              <div>
+                <button type="submit">Create Location</button>
+              </div>
+            </form>
+          </div>
+        </Modal>
       </div>
     </>
   );
