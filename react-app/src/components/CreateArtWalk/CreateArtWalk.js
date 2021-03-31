b51b2a24f0dd35934db46e1cbe45ff22f069f998
rob <darabandev@gmail.com>
2021-03-31 12:20:24 -0400

    Added footer to all pages

--- react-app/src/components/CreateArtWalk/CreateArtWalk.js
+++ react-app/src/components/CreateArtWalk/CreateArtWalk.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
-import { useHistory } from 'react-router-dom'
-import {GoogleMap, Marker, InfoWindow} from "@react-google-maps/api";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import { useDispatch, useSelector } from "react-redux";
 import Modal from "react-modal";
 import * as locationActions from "../../store/locations";
 import * as artWalkActions from "../../store/artwalks";
@@ -9,37 +9,37 @@ import * as artWalkActions from "../../store/artwalks";
 import mapStyle from "../Maps/mapStyle.js";
 import ArtCard from "../ArtCard/ArtCard.js";
 import "../ArtCard/artcard.css";
-import './creatArtWalk.css';
+import "./creatArtWalk.css";
 import "@reach/combobox/styles.css";
+import Footer from "../Footer";
 
 const customStyles = {
-    content : {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      padding: "1.5em",
-      backgroundColor: "rgba(254, 58, 158, .7)",
-      borderRadius: "2px",
-      border: "none",
-      width: "40%",
-      boxSizing: "border-box",
-
-    },
-    overlay : {
-        // backgroundColor: "transparent",
-        backgroundColor: "rgba(0, 0, 0, .6)",
-        zIndex: "100",
-    }
-  };
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "1.5em",
+    backgroundColor: "rgba(254, 58, 158, .7)",
+    borderRadius: "2px",
+    border: "none",
+    width: "40%",
+    boxSizing: "border-box",
+  },
+  overlay: {
+    // backgroundColor: "transparent",
+    backgroundColor: "rgba(0, 0, 0, .6)",
+    zIndex: "100",
+  },
+};
 
 const mapContainerStyle = {
   height: "88vh",
   width: "80vw",
   // marginLeft: "20em",
-  float: "left"
+  float: "left",
 };
 const options = {
   styles: mapStyle,
@@ -51,151 +51,158 @@ const center = {
   lng: -97.74073530134736,
 };
 
+export default function CreateArtWalk() {
+  const { allLocations } = useSelector(state => state.locations);
+  const locations = useSelector(state => Object.values(state.locations.allLocations));
+  const sessionUser = useSelector(state => state.session.user);
 
+  const [newArtWalk, setNewArtWalk] = useState(false);
+  const [artWalkName, setArtWalkName] = useState("");
+  const [showModal, setShowModal] = useState(true);
+  const [loaded, setLoaded] = useState(false);
+  const [markers, setMarkers] = React.useState([]);
+  const [artWalkList, setArtWalkList] = useState([]);
+  const [selected, setSelected] = React.useState(null);
 
-export default function CreateArtWalk(){
-    const {allLocations} = useSelector((state) => state.locations)
-    const locations = useSelector((state) => Object.values(state.locations.allLocations));
-    const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const [newArtWalk, setNewArtWalk] = useState(false)
-    const [artWalkName, setArtWalkName] = useState('');
-    const [showModal, setShowModal] = useState(true);
-    const [loaded, setLoaded] = useState(false);
-    const [markers, setMarkers] = React.useState([]);
-    const [artWalkList, setArtWalkList] = useState([]);
-    const [selected, setSelected] = React.useState(null);
+  useEffect(() => {
+    dispatch(locationActions.getAllLocations());
+    setLoaded(true);
+  }, [dispatch]);
 
+  const onClick = () => {
+    setShowModal(false);
+  };
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const onMapClick = React.useCallback(e => {
+    setMarkers(current => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
 
-    useEffect(() => {
-        dispatch(locationActions.getAllLocations());
-        setLoaded(true)
-        }, [dispatch]);
+  const addToWalk = async e => {
+    const id = e.target.id.toString();
+    const location = allLocations[id];
+    setArtWalkList(artWalkList => [...artWalkList, location]);
+    setSelected(null);
+  };
 
-
-    const onClick = () => {
-        setShowModal(false)
-    }
-
-    const onMapClick = React.useCallback((e) => {
-        setMarkers((current) => [
-          ...current,
-          {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng(),
-            time: new Date(),
-          },
-        ]);
-      }, []);
-
-
-    const addToWalk = async (e) => {
-      const id = (e.target.id).toString();
-      const location = allLocations[id]
-      setArtWalkList(artWalkList => [...artWalkList, location])
-      setSelected(null)
-    }
-
-    const handleSubmit = async (e) => {
-      e.preventDefault()
-      const res = await dispatch(artWalkActions.createArtWalk({
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const res = await dispatch(
+      artWalkActions.createArtWalk({
         artWalkList,
         user_id: sessionUser.id,
-        artWalkName
-      }))
-      history.push(`/artwalks/${res.id}`)
-    }
+        artWalkName,
+      })
+    );
+    history.push(`/artwalks/${res.id}`);
+  };
 
-      const mapRef = React.useRef();
-      const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-      }, []);
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback(map => {
+    mapRef.current = map;
+  }, []);
 
-      const panTo = React.useCallback(({ lat, lng }) => {
-        mapRef.current.panTo({ lat, lng });
-        mapRef.current.setZoom(14);
-      }, []);
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
 
   return (
     <>
       <Modal style={customStyles} isOpen={showModal}>
-          <form onSubmit={onClick}>
-              <div>
-                  <h2>Create A Name For Your Walk</h2>
-                  <input
-                  type='text'
-                  placeholder='Art Walk Name'
-                  value={artWalkName}
-                  onChange={(e) => setArtWalkName(e.target.value)}
-                  >
-                  </input>
-              </div>
-              <div>
-                  <button type="submit" disabled={artWalkName.length ? false : true}>Enter</button>
-              </div>
-          </form>
+        <form onSubmit={onClick}>
+          <div>
+            <h2>Create A Name For Your Walk</h2>
+            <input
+              type="text"
+              placeholder="Art Walk Name"
+              value={artWalkName}
+              onChange={e => setArtWalkName(e.target.value)}
+            ></input>
+          </div>
+          <div>
+            <button type="submit" disabled={artWalkName.length ? false : true}>
+              Enter
+            </button>
+          </div>
+        </form>
       </Modal>
-      <div className='main'>
+      <div className="main">
         <div className="artMapPageContainer">
           <div id="artWalkCardList">
             <h1>New Art Walk: {artWalkName}</h1>
-            <button type="submit" disabled={!artWalkList.length || artWalkList.length > 10} onClick={handleSubmit}>Get Walkin!</button>
-              {artWalkList.length > 10 &&
-              <h2>You have added too many artwalks</h2>}
-              {artWalkList && artWalkList.map((location) => (
-                <div className='artWalkCard'>
+            <button type="submit" disabled={!artWalkList.length || artWalkList.length > 10} onClick={handleSubmit}>
+              Get Walkin!
+            </button>
+            {artWalkList.length > 10 && <h2>You have added too many artwalks</h2>}
+            {artWalkList &&
+              artWalkList.map(location => (
+                <div className="artWalkCard">
                   {console.log(location)}
-                  <ArtCard location={location}/>
+                  <ArtCard location={location} />
                 </div>
-                ))
-              }
+              ))}
           </div>
           <div className="allArtMapContainer">
             <GoogleMap
-                id="map"
-                mapContainerStyle={mapContainerStyle}
-                zoom={12}
-                center={center}
-                options={options}
-                onClick={onMapClick}
-                onLoad={onMapLoad}
+              id="map"
+              mapContainerStyle={mapContainerStyle}
+              zoom={12}
+              center={center}
+              options={options}
+              onClick={onMapClick}
+              onLoad={onMapLoad}
             >
-            {locations.length > 0 && locations.map((location) => (
-              <Marker
-              key={location.id}
-              position={{lat: location.lat, lng: location.long}}
-              onClick={() => {
-                  setSelected(location);
-              }}
-              icon={{
-                  scaledSize: new window.google.maps.Size(30,30),
-                  origin: new window.google.maps.Point(0, 0),
-                  anchor: new window.google.maps.Point(15, 15)
-                }}
-              />
-            ))}
+              {locations.length > 0 &&
+                locations.map(location => (
+                  <Marker
+                    key={location.id}
+                    position={{ lat: location.lat, lng: location.long }}
+                    onClick={() => {
+                      setSelected(location);
+                    }}
+                    icon={{
+                      scaledSize: new window.google.maps.Size(30, 30),
+                      origin: new window.google.maps.Point(0, 0),
+                      anchor: new window.google.maps.Point(15, 15),
+                    }}
+                  />
+                ))}
 
-            {selected && (
-              <InfoWindow
-                onCloseClick={() => {
+              {selected && (
+                <InfoWindow
+                  onCloseClick={() => {
                     setSelected(null);
-                }}
-                position={{lat: selected.lat, lng: selected.long}}
-              >
-                <div>
-                  <img src={selected.photos[0].url} alt='wallArt' style={{height: "300px", width: "300px"}}/>
-                  <p><b>Address: {selected.street_address}, {selected.city}, {selected.state}, {selected.zip_code}</b></p>
-                  <button id={selected.id} onClick={addToWalk}>Add to Walk</button>
-                </div>
-              </InfoWindow>
-            )}
+                  }}
+                  position={{ lat: selected.lat, lng: selected.long }}
+                >
+                  <div>
+                    <img src={selected.photos[0].url} alt="wallArt" style={{ height: "300px", width: "300px" }} />
+                    <p>
+                      <b>
+                        Address: {selected.street_address}, {selected.city}, {selected.state}, {selected.zip_code}
+                      </b>
+                    </p>
+                    <button id={selected.id} onClick={addToWalk}>
+                      Add to Walk
+                    </button>
+                  </div>
+                </InfoWindow>
+              )}
             </GoogleMap>
           </div>
         </div>
       </div>
+      <Footer bottomOfPage={true} />
     </>
-)
+  );
 }
--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,26 +1,28 @@
 import "./Home.css";
-import splashImage from "./Header_Dragon_Skeleton.jpg"
-import modalImage from "./Header_Hair_Blowing.jpg"
+import splashImage from "./Header_Dragon_Skeleton.jpg";
+import modalImage from "./Header_Hair_Blowing.jpg";
+import Footer from "../Footer";
 
-export default function Home({display}) {
+export default function Home({ display }) {
   return (
     <>
-    <div className="main home_main">
-      {display && <div className="splash-container">
-          <h1 className="title header">Connecting Communities Through Public Art</h1>
-          <div className="subtitle">
-            <h3 className="subtitle1">in Austin, <span className="subtitle2">Texas</span></h3>
+      <div className="main home_main">
+        {display && (
+          <div className="splash-container">
+            <h1 className="title header">Connecting Communities Through Public Art</h1>
+            <div className="subtitle">
+              <h3 className="subtitle1">
+                in Austin, <span className="subtitle2">Texas</span>
+              </h3>
+            </div>
           </div>
+        )}
+        {display && <img className="header__img" src={splashImage} alt="Colorful mural with dragon" />}
+        {!display && (
+          <img className="header__img" src={modalImage} alt="Colorful mural with woman's hair blowing in wind" />
+        )}
       </div>
-      }
-      {display &&
-
-      <img className="header__img" src={splashImage} alt="Colorful mural with dragon"/>
-    }
-      {!display &&
-      <img className="header__img" src={modalImage} alt="Colorful mural with woman's hair blowing in wind"/>
-    }
-    </div>
+      <Footer bottomOfPage={true} />
     </>
   );
 }
