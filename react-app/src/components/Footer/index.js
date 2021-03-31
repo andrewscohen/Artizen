e562f85839514e51a2813cab2f324d77f4f5c198
rob <darabandev@gmail.com>
2021-03-31 09:16:33 -0400

    Set up footer structure

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,7 +3,42 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <div className="footer-section">
+        <p>Andrew Cohen</p>
+        <a href="#">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+      <div className="footer-section">
+        <p>Austin Howard</p>
+        <a href="#">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+      <div className="footer-section">
+        <p>Matt Kufchak</p>
+        <a href="#">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
+      <div className="footer-section">
+        <p>Rob Daraban</p>
+        <a href="#">
+          <i class="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-linkedin"></i>
+        </a>
+      </div>
     </div>
   );
 };
